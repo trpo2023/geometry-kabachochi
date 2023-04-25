17ca9a30b8bdb5443b3de9d540855502f8894d8d
Egor_Gretskiy <gretskiy.egor@mail.ru>
2023-04-25 19:40:01 +0700

    Add new structures

--- src/libgeometry/calculator.h
+++ src/libgeometry/calculator.h
@@ -1,7 +1,27 @@
 #pragma once
 
+typedef struct Point
+{
+    double x;
+    double y;
+} point;
+
+typedef struct Circle
+{
+    point center;
+    double radius;
+    double perimetr;
+    double square;
+} circle;
+
 double circle_perimetr(double radius);
 
 double circle_square(double radius);
 
-void print_circle(double square, double perimetr);
+double point_distance(const point *first, const point *second);
+
+int circle_collision(const circle *first, const circle *second);
+
+void print_circle(circle *result);
+
+void print_name(circle *result);

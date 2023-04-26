92196efc2357dc2c73f117ef0d1fbdec914c23ea
Egor_Gretskiy <gretskiy.egor@mail.ru>
2023-04-26 09:11:42 +0700

    Fix leak memory

--- test/parser_test.c
+++ test/parser_test.c
@@ -10,11 +10,13 @@ CTEST(parser, string_to_double)
 {
     double real[3] = {2, 3, -5};
     char string[] = "2 3, -5";
-    double* expected = malloc(3 * sizeof(double));
+    double* expected;
 
     expected = string_to_double(string);
 
     ASSERT_DBL_NEAR(expected[0], real[0]);
     ASSERT_DBL_NEAR(expected[1], real[1]);
     ASSERT_DBL_NEAR(expected[2], real[2]);
+
+    free(expected)
 }
